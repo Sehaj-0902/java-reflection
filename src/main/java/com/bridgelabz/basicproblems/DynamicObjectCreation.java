2b1afd7d6b7cebcc93d972d47898a742030aac12
Sehaj-0902 <sehajpreet1296.be21@chitkara.edu.in>
2025-04-07 10:18:05 +0530

    [Sehaj] ADD: Program to Dynamically Create Objects: Write a program to create an instance of a Student class dynamically using Reflection without using the new keyword.

---
+++ src/main/java/com/bridgelabz/basicproblems/DynamicObjectCreation.java
@@ -0,0 +1,31 @@
+package com.bridgelabz.basicproblems;
+
+import java.lang.reflect.Constructor;
+
+class StudentInfo {
+    private String name;
+
+    public StudentInfo(String name) {
+        this.name = name;
+    }
+
+    public void displayName() {
+        System.out.println("Student Name: " + name);
+    }
+}
+
+public class DynamicObjectCreation {
+    public static void main(String[] args) throws Exception {
+        Class<?> cls = StudentInfo.class;
+
+        Constructor<?> constructor = cls.getConstructor(String.class);
+        StudentInfo student = (StudentInfo) constructor.newInstance("Sehaj");
+
+        student.displayName();
+    }
+}
+
+/*
+Output:
+    Student Name: Sehaj
+ */
